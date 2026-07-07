63aa6c5f1ace9456b5dfbc6c1a14ff06d1ad25f8
Anton Shestakov <av6@dwimlabs.net>
2026-07-06 23:27:19 -0300

    lib: add docstrings

--- src/eui48_bit_distance/lib.py
+++ src/eui48_bit_distance/lib.py
@@ -1,3 +1,5 @@
+""" Calculate distance in bits between EUI-48 (a.k.a. 48-bit MAC) strings. """
+
 from string import hexdigits
 
 EUI48_OCTETS = 48 // 8
@@ -6,6 +8,7 @@ EUI48_MAX = 2 ** 48 - 1  # it's not a magic number if it's in the name
 
 
 def check_eui48(a):
+    """ Check if input is a EUI-48 string. """
     if len(a) != EUI48_LENGTH:
         return False
     isep = frozenset(range(2, EUI48_LENGTH, 3))
@@ -19,23 +22,27 @@ def check_eui48(a):
 
 
 def int_to_eui48(i):
+    """ Convert int into EUI-48 string. """
     if 0 <= i <= EUI48_MAX:
         return i.to_bytes(6, byteorder='big', signed=False).hex(':')
     raise ValueError('this number cannot be represented as EUI-48')
 
 
 def eui48_to_int(string):
+    """ Convert EUI-48 string into int. """
     octets = [int(s, 16) for s in string.split(':')]
     return int.from_bytes(octets, byteorder='big', signed=False)
 
 
 def eui48_bit_distance(a, b):
+    """ Calculate bit distance between two EUI-48 strings. """
     ia = eui48_to_int(a)
     ib = eui48_to_int(b)
     return (ia ^ ib).bit_count()
 
 
 def eui48_find_similar(a, iterable, cutoff=8):
+    """ Return EUI-48 strings and their differences from reference EUI-48 string. """
     result = (
         (eui48_bit_distance(a, item), item)
         for item in iterable
