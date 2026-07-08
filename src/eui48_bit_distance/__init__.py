3e64f47234bf6c684e2ee2bc804c22ca208c0feb
Anton Shestakov <av6@dwimlabs.net>
2026-07-08 02:03:47 -0300

    lib: use __all__ to expose all our public code

--- src/eui48_bit_distance/__init__.py
+++ src/eui48_bit_distance/__init__.py
@@ -6,3 +6,13 @@ from .lib import (
     eui48_to_int,
     int_to_eui48,
 )
+
+
+__all__ = [
+    'EUI48_MAX',
+    'check_eui48',
+    'eui48_bit_distance',
+    'eui48_find_similar',
+    'eui48_to_int',
+    'int_to_eui48',
+]
