db749835583bd08e3d4d6be092b6c052a0b9729e
Anton Shestakov <av6@dwimlabs.net>
2026-07-31 17:37:55 -0300

    lib: improve docstrings, document new format support

--- src/eui48_bit_distance/lib.py
+++ src/eui48_bit_distance/lib.py
@@ -11,9 +11,11 @@ EUI48_SEPARATORS = frozenset({'-', ':'})
 
 
 def check_eui48(a: str, /) -> bool:
-    """ Check if input is a EUI-48 string.
+    """ Check if the input is an EUI-48 identifier.
 
-    :return: ``True`` if `a` is a EUI-48 string, otherwise ``False``.
+    Supported formats: ``12:34:56:78:90:ab`` and ``12-34-56-78-90-ab``. Letters can be in upper or lower case.
+
+    :return: ``True`` if `a` is an EUI-48 identifier, otherwise ``False``.
     """
     if len(a) != EUI48_LENGTH:
         return False
@@ -33,10 +35,13 @@ def check_eui48(a: str, /) -> bool:
 
 
 def int_to_eui48(i: int, /, *, sep: str = ':') -> str:
-    """ Convert int into EUI-48 string.
+    """ Convert an integer into an EUI-48 identifier.
+
+    Supported formats: ``12:34:56:78:90:ab`` and ``12-34-56-78-90-ab``. Set `sep` to either ':' (the default value) or
+    '-'.
 
     :raises ValueError: If the provided value cannot be represented by EUI-48.
-    :return: `i` converted from ``int`` into EUI-48 string.
+    :return: `i` converted from ``int`` into EUI-48 identifier.
     """
     if 0 <= i <= EUI48_MAX:
         return i.to_bytes(6, byteorder='big', signed=False).hex(sep)
@@ -44,9 +49,14 @@ def int_to_eui48(i: int, /, *, sep: str = ':') -> str:
 
 
 def eui48_to_int(a: str, /) -> int:
-    """ Convert EUI-48 string into int.
+    """ Convert an EUI-48 identifier into an integer.
 
-    :return: `a` converted from EUI-48 string into ``int``.
+    Supported formats: ``12:34:56:78:90:ab`` and ``12-34-56-78-90-ab``. Letters can be in either upper or lower case.
+
+    No checks are performed on the input: if the provided string is not an EUI-48 identifier, this function will produce
+    an error. Use func:`check_eui48` first if you're not sure about the input.
+
+    :return: `a` converted from EUI-48 identifier into ``int``.
     """
     sep = a[2]
     octets = [int(s, 16) for s in a.split(sep)]
@@ -54,9 +64,9 @@ def eui48_to_int(a: str, /) -> int:
 
 
 def eui48_bit_distance(a: str, b: str, /) -> int:
-    """ Calculate bit distance between two EUI-48 strings.
+    """ Calculate bit distance between two EUI-48 identifiers.
 
-    :return: Amount of bits that are different between `a` and `b`.
+    :return: The amount of bits that are different between `a` and `b`.
     """
     ia = eui48_to_int(a)
     ib = eui48_to_int(b)
@@ -64,12 +74,12 @@ def eui48_bit_distance(a: str, b: str, /) -> int:
 
 
 def eui48_find_similar(a: str, iterable: Iterable[str], /, *, cutoff: int = 8) -> list[tuple[int, str]]:
-    """ Return EUI-48 strings and their distances from reference EUI-48 string.
+    """ Return EUI-48 identifiers and their distances from the reference EUI-48 identifier.
 
     This function measures and orders output by the distances between `a` and every string in `iterable`. To reduce the
     number of elements in the output, it also drops inputs that have distance greater than `cutoff`.
 
-    :return: A list of 2-tuples ``(distance, EUI-48 string)``.
+    :return: A list of 2-tuples ``(distance, EUI-48 identifier)``.
     """
     result = (
         (eui48_bit_distance(a, item), item)
