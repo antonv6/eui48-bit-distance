042d4329799b3939f73f7cce749a4dbf9093ac9e
Anton Shestakov <av6@dwimlabs.net>
2026-08-09 19:41:31 -0300

    tests: benchmark bytes_to_eui48() and eui48_to_bytes()

--- tests/test_benchmark.py
+++ tests/test_benchmark.py
@@ -1,14 +1,24 @@
 from src.eui48_bit_distance import (
+    bytes_to_eui48,
     check_eui48,
     eui48_bit_distance,
     eui48_find_similar,
+    eui48_to_bytes,
     eui48_to_int,
     int_to_eui48,
 )
 
 
+def test_bench_eui48_to_bytes(benchmark):
+    benchmark(eui48_to_bytes, '12:34:56:78:90:ab')
+
+
 def test_bench_eui48_to_int(benchmark):
     benchmark(eui48_to_int, '12:34:56:78:90:ab')
+
+
+def test_bench_bytes_to_eui48(benchmark):
+    benchmark(bytes_to_eui48, bytes([0x12, 0x34, 0x56, 0x78, 0x90, 0xab]))
 
 
 def test_bench_int_to_eui48(benchmark):
